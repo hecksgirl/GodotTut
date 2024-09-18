50fc3d1904085b2c0f0885581567583c52cc63d7
Tori Meyers <159385699+hecksgirl@users.noreply.github.com>
2024-09-18 18:25:06 -0500

    9-18

--- player.gd
+++ player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,13 +17,25 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction == -1: # If user moves left flip the sprite
+		get_node("AnimatedSprite2D").flip_h = true
+	elif direction == 1: # If user moves right flip the sprite
+		get_node("AnimatedSprite2D").flip_h = false	
 		velocity.x = direction * SPEED
+	if direction:
+		velocity.x = direction * SPEED	
+		if velocity.y == 0:
+			anim.play("run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, SPEED) #Idle animation when not moving
+		if velocity.y == 0:
+				anim.play("idle")
+	if velocity.y > 0: # Plays fall animation if sprite is moving positive on y axis
+		anim.play("fa'")
+	
 	move_and_slide()
